ceb993198f2616046d10972961e38a073555fd7e
Triodin <ufolkin1@gmail.com>
2017-05-15 04:59:26 +0300

    start creating graphic engine

--- src/include/render.h
+++ src/include/render.h
@@ -5,12 +5,28 @@
 #ifndef PROGBASE3_RENDER_H
 #define PROGBASE3_RENDER_H
 
-#include "SDL.h"
+#include <SDL.h>
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#define max(a,b) (((a) > (b)) ? (a) : (b))
 
-class render {
+static struct sector {
+    double floor, ceil;
+    struct xy {double x, y;} *vertex;
 
-};
+} *sectors = NULL;
+
+static struct player {
+    struct xyz {double x, y, z;} where, velocity;
+    unsigned int sector;
+} player;
+
+static SDL_Surface *surface = NULL;
 
 
 #endif //PROGBASE3_RENDER_H
