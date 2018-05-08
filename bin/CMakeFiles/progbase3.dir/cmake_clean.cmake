90206aa4ad72d63ebddd6137faba4e4c2eead417
Triodin <ufolkin1@gmail.com>
2018-05-08 22:33:14 +0300

    refresh

---
+++ bin/CMakeFiles/progbase3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progbase3.dir/main.c.o"
+  "CMakeFiles/progbase3.dir/src/render.c.o"
+  "CMakeFiles/progbase3.dir/src/data_loader.c.o"
+  "CMakeFiles/progbase3.dir/src/player_func.c.o"
+  "progbase3.pdb"
+  "progbase3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/progbase3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
